904e097b5bd56c439a8b3e7876193d1449581192
SimonSchreinermacher <49522875+SimonSchreinermacher@users.noreply.github.com>
2021-03-28 20:35:28 +0200

    Added Input fields to communicate with Backend database

--- MERN with Next.js/client/src/App.js
+++ MERN with Next.js/client/src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      name : "",
+      password: "",
+      email : "",
+      userData : []
+    }
+  }
+
+  handleSubmit(){
+    let newUser = {name: this.state.name, password: this.state.password, email: this.state.email};
+    axios.post("http://localhost:3001/user", newUser)
+      .then(res => {
+        console.log(JSON.stringify(res.data));
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  getAllEntries(){
+    axios.get("http://localhost:3001/user")
+      .then((res) => {
+        this.setState({userData: res.data});
+        for(let x of this.state.userData){
+          console.log(x);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <form onSubmit={this.getAllEntries.bind(this)}>
+          <button type="submit">Get all entries</button>
+        </form>
+        <form onSubmit = { this.handleSubmit.bind(this)}>
+          <div>
+            <p>Name:</p>
+            <input onChange={(e) => this.setState({name: e.target.value})}></input>
+          </div>
+          <div>
+            <p>Password:</p>
+            <input onChange={(e) => this.setState({password: e.target.value})}></input>
+          </div>
+          <div>
+            <p>Email:</p>
+            <input onChange={(e) => this.setState({email: e.target.value})}></input>
+          </div>
+          <button type="submit">Add to database</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
